dc412c7e6c658f006354188292904c476eb46722
Tomas Racek <tom@krab1k.net>
2016-01-29 17:13:13 +0100

    doladeno, ze vypocet probehne, jeste ale nezobrazi

--- server/optim_job.sh
+++ server/optim_job.sh
@@ -27,7 +27,37 @@ structures_count="$(find . -name '*.pdb' | wc -l)"
 PATH=${SCRATCHDIR}:$PATH
 TMPDIR=${SCRATCHDIR}
 export TMPDIR
-mpirun ./ensamble-fit -n "${structures_count}" -m saxs.dat -s "${OPTIM_STEPS}" -y "${OPTIM_SYNC_STEPS}" -l "${OPTIM_PARAM_ALPHA}" -b "${OPTIM_PARAM_BETA}" -g "${OPTIM_PARAM_GAMMA}"  -L -p structure00 >> "${request_dir}/results"
+mpirun ./ensamble-fit -n "${structures_count}" -m saxs.dat -s "${OPTIM_STEPS}" -y "${OPTIM_SYNC_STEPS}" -l "${OPTIM_PARAM_ALPHA}" -b "${OPTIM_PARAM_BETA}" -g "${OPTIM_PARAM_GAMMA}"  -L -p structure00 >> "${request_dir}/results" &
+
+pid=$!
+
+while ps p $pid >/dev/null 2>/dev/null; do
+	sleep $OPTIM_JOBS_CHECK_INTERVAL
+	if test -f progress.dat; then
+		cp progress.dat ${request_dir}
+		set -- `grep c12: progress.dat`
+		e=$2 w=$3
+
+		mkdir ComputedCurves
+		cd ComputedCurves
+		ln ../saxs.dat .
+		for i in `seq 1 $structures_count`; do
+			n=`printf %02d $i`
+			ln ../structure00$n.pdb .
+			foxs -e $e -w $w structure00$n.pdb saxs.dat
+			grep -v '^#' structure00${n}_saxs.dat | while read l; do
+				set -- $l
+				echo $1 $3 0.0 >>final_m${i}.pdb.dat 
+			done 
+		done
+		cd ..
+		tar cf ComputedCurves.tar ComputedCurves/final_m*.dat
+		mv ComputedCurves.tar progress.dat ${request_dir}
+		rm -rf ComputedCurves.tar ComputedCurves
+	fi
+done
+
+wait $pid
 retval="$?"
 
 # clean things up
